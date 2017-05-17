153b552eee52fcc460fffc9b862e47b97dcb312e
oduvanio <oduvanio@mail.ru>
2017-05-17 23:08:33 +0400

    Update button-scroll-up.js

--- button-scroll-up.js
+++ button-scroll-up.js
@@ -1,3 +1,24 @@
-var divButtonScrollUp = document.createElement('div');
-divButtonScrollUp.className = "button-scroll-up";
-document.body.appendChild(divButtonScrollUp);
+$(document).ready(function() {
+	var divButtonScrollUp = document.createElement('div');
+	divButtonScrollUp.className = "button-scroll-up";
+	divButtonScrollUp.innerHTML = "/\\ Наверх";
+	document.body.appendChild(divButtonScrollUp);
+	window.onscroll = function() {
+		if(window.pageYOffset > 300) {
+			divButtonScrollUp.style.display = 'block';
+		} else {
+			divButtonScrollUp.style.display = 'none';
+		}
+	}
+	var t;
+	divButtonScrollUp.onclick = buttonScrollUp;
+});
+function buttonScrollUp() {
+	if(window.pageYOffset > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('buttonScrollUp()',20);
+	} else {
+		clearTimeout(t);
+	}
+	return false;
+}
